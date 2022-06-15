9b7f643e962e2a99ac6a7df8189b03d1811f2e26
Yurippe <yurichikawa1992@gmail.com>
2022-06-15 15:27:29 -0400

    add ability to add/remove replies to comments

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -2,7 +2,7 @@ const { Pizza, Comment } = require('../models');
 
 const commentController = {
     // add comment to pizza
-    addComment({ params, body}, res) {
+    addComment({ params, body }, res) {
         console.log(body);
         Comment.create(body)
             .then(({ _id }) => {
@@ -27,7 +27,7 @@ const commentController = {
         Comment.findOneAndDelete({ _id: params.commentId })
             .then(deletedComment => {
                 if (!deletedComment) {
-                    return res.status(404).json({ message: 'No comment with this id!' });
+                    return res.status(404).json({ message: 'No comment found with this id!' });
                 }
                 return Pizza.findOneAndUpdate(
                     { _id: params.pizzaId },
@@ -42,6 +42,33 @@ const commentController = {
                 }
                 res.json(dbPizzaData);
             })
+            .catch(err => res.json(err));
+    },
+
+    addReply({ params, body }, res) {
+        console.log(body);
+        Comment.findOneAndUpdate(
+            { _id: params.commentId },
+            { $push: { replies: body } },
+            { new: true }
+        )
+            .then(dbPizzaData => {
+                if (!dbPizzaData) {
+                    res.status(404).json({ message: 'No pizza found with this id!' });
+                    return;
+                }
+                res.json(dbPizzaData);
+            })
+            .catch(err => res.json(err));
+    },
+
+    removeReply({ params }, res) {
+        Comment.findOneAndUpdate(
+            { _id: params.commentId },
+            { $pull: { replies: { replyId: params.replyId } } },
+            { new: true }
+        )
+            .then(dbPizzaData => res.json(dbPizzaData))
             .catch(err => res.json(err));
     }
 };
